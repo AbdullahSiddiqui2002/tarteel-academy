8456a7f02e69d3cab129cd59df8fb54c1b69267c
AbdullahSiddiqui2002 <abdullah.siddiqui13122002@gmail.com>
2024-09-05 23:42:58 +0500

    Home Page Updated

--- js/main.js
+++ js/main.js
@@ -78,3 +78,20 @@
 
 })(jQuery);
 
+
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
